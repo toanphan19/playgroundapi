5e969bbca2a90fb1645f290a518736cbcc2570b6
toanphan19 <toanphan.cs@gmail.com>
2022-03-14 03:47:04 +0700

    fix: fix CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app import wordle
@@ -9,6 +10,22 @@ from app.wordle.solver import Guess
 from . import gpt3
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
